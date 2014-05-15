1813a84b467b337cbff73fc9ef6ea3c224a09d6c
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2014-05-15 14:38:01 +0530

    fb login rolling

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -3,5 +3,6 @@ class BasicController < ApplicationController
     #@e  = Entity.find_by("name"=>/bubble/i)
     #@mc = Entity.find_by("type"=>@e.type+"/crawler")
     #eval(@mc["code"])
+    puts @current_user
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,9 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
+    5.times do puts user.id end
+      puts session[:user_id].class
+      
     redirect_to root_url
   end
 
