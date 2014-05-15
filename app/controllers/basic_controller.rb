543e49389f75b97212073898cf7299e5871344d3
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2014-05-15 12:06:56 +0530

    ..

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -1,7 +1,7 @@
 class BasicController < ApplicationController
   def show
-    @e  = Entity.find_by("name"=>/bubble/i)
-    @mc = Entity.find_by("type"=>@e.type+"/crawler")
-    eval(@mc["code"])
+    #@e  = Entity.find_by("name"=>/bubble/i)
+    #@mc = Entity.find_by("type"=>@e.type+"/crawler")
+    #eval(@mc["code"])
   end
 end
