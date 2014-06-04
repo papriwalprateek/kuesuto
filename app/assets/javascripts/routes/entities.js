6f58873000adf25789b10b73ca6d42b70d91faaf
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2014-06-04 18:33:32 +0530

    debug....

--- app/assets/javascripts/routes/entities.js
+++ app/assets/javascripts/routes/entities.js
@@ -1,12 +1,6 @@
-App.EntitiesRoute = Ember.Route.extend({
-  beforeModel: function() {
-    Ember.$("body").addClass("loading");
-  },
+App.IndexRoute = Ember.Route.extend({
   model: function(params) {
     return {has: "error",error:"this"};
-  },
-  afterModel: function() {
-    Ember.$("body").removeClass("loading");
   }
 });
  
---
+++ app/assets/javascripts/routes/index.js
@@ -0,0 +1,12 @@
+App.EntitiesRoute = Ember.Route.extend({
+  beforeModel: function() {
+    Ember.$("body").addClass("loading");
+  },
+  model: function(params) {
+    return {has: "error",error:"this"};
+  },
+  afterModel: function() {
+    Ember.$("body").removeClass("loading");
+  }
+});
+ 
