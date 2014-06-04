08e62e72c66b45e87fbc25c12404fd2d916da039
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2014-06-04 14:15:20 +0530

    ember api and ui basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
- def get_contents(a)                  ### gives children or htmloutput in case of type /dqs/...
+  def get_contents(a)                  ### gives children or htmloutput in case of type /dqs/...
       a=a.downcase
       a_split= a.split('/')
       k=0
@@ -23,6 +23,12 @@ class ApplicationController < ActionController::Base
           a=p
           q=nil
       end
+      err={}
+      if (Entity.where(addr:a).count==0 && Entity.where(addr:p).where(name:/^#{Regexp.escape(q)}$/i).count==0)
+          err['error']='not found'
+          err['has']='error'
+          return err
+          else
       r=Hash.new
       if a_split.length==1
           r['has']='repos'
@@ -44,6 +50,11 @@ class ApplicationController < ActionController::Base
           r1["type"]='content'
           r1["node"]=Entity.find_by(addr:a)
           r1["title"]=r1['node']['title']
+          if(r1['node']['type']=='/dqs/videopage')
+              r1['out_type']='video'
+              else
+              r1['out_type']='text'
+          end
           r1["out_html"]=r1['node']['cache']
           r1["url"]=r1['node']['url']
           r['source']<<r1
@@ -67,16 +78,21 @@ class ApplicationController < ActionController::Base
                 if e['isleaf']
                     r['has']='tiles'
                     r['tiles']=[]
-                    e['property_list'].each do |p|
-                        if p[1]>0
-                            puts p[0]
+                    e['p_list'].each do |p,n|
+                        if n>0
+                            puts p
                             r1=Hash.new
-                            r1["tile_title"]=p[0]
+                            r1["tile_title"]=p
                             r1["tile_nodes"]=[]
-                            e[p[0]].each do |id|
+                            e[p].each do |id|
                                 r2=Hash.new
                                 r2['query']=e['addr']+'/'+e['name']+'/'+id.to_s
                                 r2['node']=Entity.find(id)
+                                if p=='video'
+                                    r2['node']['out_type']='video'
+                                    else
+                                    r2['node']['out_type']='text'
+                                end
                                 r1['tile_nodes']<<r2
                             end
                             r['tiles']<<r1
@@ -103,8 +119,9 @@ class ApplicationController < ActionController::Base
         end
     end
           return r
+          end
       end
-        
+    
   private
   def current_user
     @current_user ||= User.find(session[:user_id]["$oid"]) if session[:user_id]
