242f25e210a2b32f88c9406398dc9a961d848a6f
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2014-06-04 19:03:32 +0530

    --

--- app/assets/javascripts/routes/index.js
+++ app/assets/javascripts/routes/index.js
@@ -3,6 +3,12 @@
   model: function(params) {
   	
     return $.getJSON('/api/v1/entities.json?addr='+params.addr);
+  },
+  beforeModel: function() {
+    Ember.$("body").addClass("loading");
+  },
+  afterModel: function() {
+    Ember.$("body").removeClass("loading");
   }
 });
  
