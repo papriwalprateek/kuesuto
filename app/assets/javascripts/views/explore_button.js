1fbd4b9cd50acf6efab46af0cbe5b48cd4c040f0
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2014-06-10 15:31:25 +0530

    urls new convention

--- app/assets/javascripts/views/explore_button.js
+++ app/assets/javascripts/views/explore_button.js
@@ -2,7 +2,9 @@ App.ExploreButtonView = Ember.View.extend({
   tagName: 'a',
   attributeBindings: ['href'],
   href: function() {
-        return '#/%@'.fmt(this._context.query.toLowerCase());
+  		var url_temp = (this._context.query.toLowerCase()).split('dq/')[1];
+  		
+        return '/#/repo%@'.fmt(url_temp);
     }.property('query')
 
 });
