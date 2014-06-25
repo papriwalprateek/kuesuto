54c9c9dfc9978c120c114f9da57efde85adef63f
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2014-06-26 00:01:23 +0530

    timestamps

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List
-  include Mongoid::Document  
+  include Mongoid::Document 
+  include Mongoid::Timestamps::Short # For c_at and u_at. 
   field :name, type: String
   has_and_belongs_to_many :entities
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps::Created::Short # For c_at and u_at.
   field :provider, type: String
   field :uid, type: String
   field :name, type: String
